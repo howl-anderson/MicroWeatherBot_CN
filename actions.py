781baf56f1c1fc8b0c85ec774e35d1dcf1412791
Xiaoquan Kong <u1mail2me@gmail.com>
2019-07-02 10:42:48 +0800

    Update action function to avoid "Tried to set non existent slot xxx"

--- actions.py
+++ actions.py
@@ -1,6 +1,6 @@
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Tracker
+from rasa_sdk import Tracker, Action
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
@@ -9,20 +9,50 @@ from service.action import get_text_weather_date
 from service.normalization import text_date_to_number_date
 
 
-class WeatherForm(FormAction):
-    @staticmethod
-    def required_slots(tracker: Tracker) -> List[Text]:
-        """A list of required slots that the form has to fill"""
+# class WeatherForm(FormAction):
+#     @staticmethod
+#     def required_slots(tracker: Tracker) -> List[Text]:
+#         """A list of required slots that the form has to fill"""
+#
+#         return ["address", "date-time"]
+#
+#     def name(self) -> Text:
+#         return "action_report_weather"
+#
+#     def submit(self,
+#                dispatcher: CollectingDispatcher,
+#                tracker: Tracker,
+#                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         address = tracker.get_slot('address')
+#         date_time = tracker.get_slot('date-time')
+#
+#         date_time_number = text_date_to_number_date(date_time)
+#
+#         if isinstance(date_time_number, str):  # parse date_time failed
+#             msg = "暂不支持查询 {} 的天气".format([address, date_time_number])
+#             return [SlotSet("matches", msg)]
+#             # print(msg)
+#             # dispatcher.utter_message(msg)
+#         else:
+#             weather_data = get_text_weather_date(address, date_time,
+#                                                  date_time_number)
+#             return [SlotSet("matches", "{}".format(weather_data))]
+#             # print(weather_data)
+#             # dispatcher.utter_message(weather_data)
+#
+#         # utter submit template
+#         # dispatcher.utter_template('utter_submit', tracker)
+#         # return []
 
-        return ["address", "date-time"]
 
+class ActionReportWeather(Action):
     def name(self) -> Text:
         return "action_report_weather"
 
-    def submit(self,
-               dispatcher: CollectingDispatcher,
-               tracker: Tracker,
-               domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         address = tracker.get_slot('address')
         date_time = tracker.get_slot('date-time')
 
