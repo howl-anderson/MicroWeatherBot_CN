6e419e155f58832d7a634cff527e18b982d77b1c
Xiaoquan Kong <u1mail2me@gmail.com>
2019-08-12 11:03:01 +0800

    Update

--- install_dependency.bash
+++ install_dependency.bash
@@ -1,2 +1,4 @@
 pip install -r ./requirements.txt
 pip install rasa-x --extra-index-url https://pypi.rasa.com/simple
+pip install https://github.com/RasaHQ/rasa/archive/supervised_response_selector.zip
+pip install tensorflow~=1.14
